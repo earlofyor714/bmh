b9898c39de5a0d9bc33fd96e93435b0320d5b284
earlofyor714 <alexphillipmartinez@gmail.com>
2017-07-27 17:49:09 -0700

    properly append csv files

--- DataCleaner.py
+++ DataCleaner.py
@@ -10,9 +10,20 @@ class DataCleaner:
         self.placeholder = None
 
     def create_train_data(self):
-        training_data = []
+        training_data = None
+        print("opening files")
         for person in tqdm(os.listdir(TRAIN_DIR)):
-            print(person)
+            person_path = TRAIN_DIR + '/' + person
+            #person_data = pandas.read_csv(person_path, sep='delimiter', header=None, engine='python')
+            person_data = pandas.read_csv(person_path, sep='delimiter', engine='python')
+            # print(person_data)
+
+            if training_data is not None:
+                training_data = training_data.append(person_data)
+            else:
+                training_data = person_data
+
+        print(training_data.shape)
 
     #def combine_files(self, name):
 
