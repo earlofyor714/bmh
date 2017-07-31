7c4ce9317ad9e417466d140d0e257ff262d8c4fd
earlofyor714 <alexphillipmartinez@gmail.com>
2017-07-31 14:27:18 -0700

    add function to view lines of csv

--- DataCleaner.py
+++ DataCleaner.py
@@ -4,6 +4,7 @@ import os
 from tqdm import tqdm
 
 TRAIN_DIR = 'resources'
+EDIT_DIR = 'resources/edited'
 
 class DataCleaner:
     def __init__(self):
@@ -13,20 +14,39 @@ class DataCleaner:
         training_data = None
         print("opening files")
         for person in tqdm(os.listdir(TRAIN_DIR)):
-            person_path = TRAIN_DIR + '/' + person
-            #person_data = pandas.read_csv(person_path, sep='delimiter', header=None, engine='python')
-            person_data = pandas.read_csv(person_path, sep='delimiter', engine='python')
-            # print(person_data)
+            if '.csv' in person:
+                person_path = TRAIN_DIR + '/' + person
+                #person_data = pandas.read_csv(person_path, sep='delimiter', header=None, engine='python')
+                person_data = pandas.read_csv(person_path, sep='delimiter', engine='python')
+                # print(person_data)
 
-            if training_data is not None:
-                training_data = training_data.append(person_data)
-            else:
-                training_data = person_data
+                if training_data is not None:
+                    training_data = training_data.append(person_data)
+                else:
+                    training_data = person_data
 
         print(training_data.shape)
+        training_data.to_csv(EDIT_DIR + '/combined_data.csv', sep=';')
 
     #def combine_files(self, name):
 
+    def get_one_line(self):
+        import csv
+
+        column_names = []
+        with open('resources/tbi16037_Shortcut_all_dates.csv', newline='') as f:
+            #for row in csv.reader(f):
+            #    if len(row) > len(column_names):
+            #        column_names = row
+            #print(column_names)
+
+            reader = csv.reader(f)
+            row1 = next(reader)
+            print(row1) # type list
+            # print(len(row1))
+
+
 
 dc = DataCleaner()
-dc.create_train_data()
+#dc.create_train_data()
+dc.get_one_line()
